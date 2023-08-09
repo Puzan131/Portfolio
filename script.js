f3af38e2bd57202ca079818db57a5d45f9c3c2e0
Poojan Bhatt <poojanbhatt392@gmail.com>
2023-08-09 15:16:20 +0545

    Added  all the files

---
+++ script.js
@@ -0,0 +1,93 @@
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+locomotive();
+
+let tl = gsap.timeline();
+tl.from(".bs .headings h1",{
+  x:"100%",
+  duration:1,
+})
+tl.from(".bs .headings h5",{
+  x:"100%",
+  delay:-1,
+  duration:1,
+})
+tl.to(".bs",{
+  height:0,
+  delay:1.5,
+  duration:1,
+  ease:Circ.easeInOut,
+})
+tl.to(".bs .headings h1",{
+  y:"-100%",
+  delay:-1,
+  duration:0.5
+})
+tl.to(".bs .headings h5",{
+  y:"-500%",
+  delay:-0.5,
+  duration:0.5
+})
+
+
+function navborder() {
+  var list = document.querySelectorAll("li");
+  list.forEach((e) => {
+    e.addEventListener("click", () => {
+      list.forEach((li) => {
+        li.style.borderBottom = "none";
+      });
+      e.style.borderBottom = "2px solid white";
+    });
+  });
+}
+navborder();
+gsap.to(".hero-heading h1", {
+  x: -80,
+
+  scrollTrigger: {
+    trigger: ".headings h1",
+    scroller: ".main",
+    start: "top 20%",
+    end: "top 0%",
+    scrub: 2,
+  },
+});
+gsap.to(".hero-heading h2", {
+  x: 80,
+
+  scrollTrigger: {
+    trigger: ".headings h1",
+    scroller: ".main",
+    start: "top 20%",
+    end: "top 0%",
+    scrub: 2,
+  },
+});
